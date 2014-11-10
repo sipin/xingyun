1437e2fa7cd8ae0965499e9dc9890a29bffcd459
dworld <dworld2006@gmail.com>
2014-11-10 13:32:42 +0800

    add xsrf error check

--- context_xsrf.go
+++ context_xsrf.go
@@ -1,6 +1,15 @@
 package xingyun
 
+import "fmt"
+
+func (ctx *Context) checkXSRF() {
+	if ctx.xsrf == nil {
+		panic(fmt.Errorf("xsrf not inited"))
+	}
+}
+
 func (ctx *Context) XSRFToken() string {
+	ctx.checkXSRF()
 	if ctx.xsrf == nil {
 		return ""
 	}
@@ -8,6 +17,7 @@ func (ctx *Context) XSRFToken() string {
 }
 
 func (ctx *Context) XSRFFormField() string {
+	ctx.checkXSRF()
 	return "<input type=\"hidden\" name=\"" + formatName(ctx.xsrf.GetFormName()) + "\" value=\"" +
 		ctx.XSRFToken() + "\"/>"
 }
